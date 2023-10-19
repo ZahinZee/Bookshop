3d8ca9b91d957b72a31875a74039c2b804338424
ZahinZee <137389784+ZahinZee@users.noreply.github.com>
2023-10-19 19:39:56 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,112 @@
+<?php
+
+include 'config.php';
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+
+      $row = mysqli_fetch_assoc($select_users);
+
+      if($row['user_type'] == 'admin'){
+
+         $_SESSION['admin_name'] = $row['name'];
+         $_SESSION['admin_email'] = $row['email'];
+         $_SESSION['admin_id'] = $row['id'];
+         header('location:admin_page.php');
+
+      }elseif($row['user_type'] == 'user'){
+
+         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['user_email'] = $row['email'];
+         $_SESSION['user_id'] = $row['id'];
+         header('location:home.php');
+
+      }
+
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <link rel="stylesheet" href="login.css">
+   <!--<link rel="stylesheet" href="css/style.css">-->
+
+</head>
+<body>
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+   
+<!--<div class="form-container">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <input type="email" name="email" placeholder="enter your email" required class="box">
+      <input type="password" name="password" placeholder="enter your password" required class="box">
+      <input type="submit" name="submit" value="login now" class="btn">
+      <p>don't have an account? <a href="register.php">register now</a></p>
+   </form>
+
+</div>-->
+
+<div class="box login_box">
+    <span class="borderline"></span>
+    <form action="" method="post">
+    <h2>Login</h2>
+
+        <div class="inputbox">
+            <input type="email" name="email" required="required">
+            <span>Email</span>
+            <i></i>
+        </div>
+
+        <div class="inputbox">
+            <input type="password" name="password" required="required">
+            <span>Password</span>
+            <i></i>
+        </div>
+        
+        <div class="links">
+            <a href="#">Forgot Password</a>
+            <a href="register.php">Sign in</a>
+        </div>
+
+        <input type="submit" value="Login" name="submit">
+    </form>
+</div>
+<script src="https://kit.fontawesome.com/eedbcd0c96.js" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
