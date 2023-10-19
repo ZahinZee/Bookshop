ad5d5bf9411953806bdd097e971da69cce7b965b
ZahinZee <137389784+ZahinZee@users.noreply.github.com>
2023-10-19 20:29:41 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+        <img src="book_logo.png" alt="">
+      <a href="home.php" class="book_logo">PickABook</a>
+         
+      <p><i class="fas fa-phone"></i> 012356789</p>
+      <p><i class="fas fa-envelope"></i> pickabook@gmail.com</p>
+      <p><i class="fas fa-map-marker-alt"></i> Mohakhali, Dhaka - 1250</p>
+    </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </p>
+
+</section>
